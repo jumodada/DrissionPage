b158aea02c1668c677bf69a2db855f9fc4093eb3
wukunyun <864353360@qq.com>
2026-06-11 17:36:16 +0800

    fix: 修复截图在iframe下的一系列问题

--- DrissionPage/_elements/chromium_element.py
+++ DrissionPage/_elements/chromium_element.py
@@ -422,7 +422,9 @@ class ChromiumElement(DrissionElement):
 
     def _find_elements(self, locator, timeout, index=1, relative=False, raise_err=None):
         self.owner.wait.doc_loaded()
-        return find_in_chromium_ele(self, locator, index, timeout, relative=relative)
+        result = find_in_chromium_ele(self, locator, index, timeout, relative=relative)
+        frame_owner = getattr(self, '_screenshot_frame_owner', None)
+        return frame_owner._mark_frame_elements(result) if frame_owner else result
 
     def style(self, style, pseudo_ele=''):
         if pseudo_ele:
@@ -515,7 +517,9 @@ class ChromiumElement(DrissionElement):
             end_time = perf_counter() + self.timeout
             while not self._run_js(js) and perf_counter() < end_time:
                 sleep(.05)
-        if scroll_to_center:
+
+        screenshot_owner = getattr(self, '_screenshot_frame_owner', None) or self.owner
+        if scroll_to_center and screenshot_owner is self.owner:
             self.scroll.to_see(center=True)
 
         left, top = self.rect.location
@@ -525,8 +529,8 @@ class ChromiumElement(DrissionElement):
         if not name:
             name = f'{self.tag}.jpg'
 
-        return self.owner._get_screenshot(path, name, as_bytes=as_bytes, as_base64=as_base64, full_page=False,
-                                          left_top=left_top, right_bottom=right_bottom, ele=self)
+        return screenshot_owner._get_screenshot(path, name, as_bytes=as_bytes, as_base64=as_base64, full_page=False,
+                                                left_top=left_top, right_bottom=right_bottom, ele=self)
 
     def input(self, vals, clear=False, by_js=False):
         if self.tag == 'input' and self.attr('type') == 'file':
